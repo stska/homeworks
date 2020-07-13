8584f883d6f9bbaba3c1d7984addc06d26e6a06c
stska <33188480+stska@users.noreply.github.com>
2020-07-13 09:17:20 +0300

    dz6
    dz6

--- MyFirstActivity/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyFirstActivity/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,97 +1,20 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-//import androidx.appcompat.widget.SearchView;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
 import android.annotation.SuppressLint;
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.CompoundButton;
-import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.SearchManager;
-import android.widget.SearchView;
-import android.widget.SearchView.OnQueryTextListener;
-
-import java.util.List;
+//import androidx.appcompat.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
 
     //данные для подргуки в избранное. Особой роли не играбт, так как делал для тренировки. Потом буду подгружать туда данные из поиска
-    String[] cityNamesArray = {
-            "Moscow",
-            "Saint-Petersburg",
-            "Warsaw",
-            "New York",
-            "Sydney",
-            "Tokyo"
-    };
-    Integer[] imagesArray = {
-            R.drawable.rainbow,
-            R.drawable.sun,
-            R.drawable.wind,
-            R.drawable.winter,
-            R.drawable.sunglasses,
-            R.drawable.moon
-    };
-    String[] weatherArray = {
-            "Rainy",
-            "Sunny",
-            "Windy",
-            "Frosty",
-            "Heat",
-            "Mild"
-    };
-    String[] temperatureArray = {
-            "15",
-            "24",
-            "10",
-            "-10",
-            "+40",
-            "+5"
-    };
-    String[] humidityArray = {
-            "59 %",
-            "65 %",
-            "45 %",
-            "50 %",
-            "80 %",
-            "76 %"
-    };
-    String[] pressureArray = {
-            "743 мм. р",
-            "600 мм. р",
-            "845 мм. р",
-            "665 mm р",
-            "720 mm p",
-            "7600 mm p"
-    };
+    public void testMeth(){
 
-    ListView listView;
-    SearchView searchView;
-
-
-    static String instanceState = null;     //переменная для выводо данных первый ли это oncreate или нет
-    static int counter = 1;           //счетчик сколько раз упере пересоздали активити
-
-    //singleton
-    DataStrorage test = DataStrorage.getData();
-
+    }
 
     @SuppressLint("WrongViewCast")
     @Override
@@ -134,14 +57,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
 
-
-
-
         int count = getSupportFragmentManager().getBackStackEntryCount();
 
         if (count == 0) {
             super.onBackPressed();
-
 
             //additional code
         } else {
@@ -150,70 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+
 }
-/*
 
-        final TextView textView = findViewById(R.id.textTestExample);         //текстовое поле в которое будут записываться данные из поиска
-        textView.setText(test.data); //запись данныхв это текстовое поля под поиском. Сейчас туда запишутся данные из синглтона, дефолтеное. В нашем случае просто "Test"
-
-        searchView = (SearchView) findViewById(R.id.searchViewId);
-
-        //обработка ввода текста и его отправка
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                  //пробегаемся по списку имебщихся город и сравниваем с тем, что ввели. Я понимаю, что нужен некий фильтр еще здесь, но пока  так
-                for (String city : cityNamesArray) {
-                    if (city.equals(query)) {
-                        textView.setText(query);
-                        test.setData(query);  //записываем результат поиска в класс реальзованный по паттерну singleton
-                        Intent app = new Intent(MainActivity.this,ChoosenCityActivity.class);
-                        app.putExtra("city",query);
-                        startActivity(app);
-                        return false;
-                    } else {
-                        Toast.makeText(MainActivity.this, "No Match found", Toast.LENGTH_LONG).show();
-                    }
-
-                }
-                test.setData("No Match found");     //записываем результат поиска в класс реальзованный по паттерну singleton
-                textView.setText(test.data);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-        });
-
-          //блок проверов для последующего вывода результата в лог и Toast
-        if (instanceState == null) {
-            instanceState = "Первый запуск!";
-        } else {
-            counter++;
-            instanceState = "Запуск номер: " + counter;
-
-        }
-
-
-        CustomList customList = new CustomList(this, imagesArray, cityNamesArray, weatherArray, temperatureArray, humidityArray, pressureArray);
-        listView = (ListView) findViewById(R.id.favListView);
-        listView.setAdapter(customList);
-
-
-
-        listView.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent app = new Intent(MainActivity.this,ChoosenCityActivity.class);
-
-                startActivity(app);
-            }
-        });
-
-
-        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_SHORT).show();
-        Log.d(MainActivity.class.getName(), instanceState + " - onCreate()");
-
- */
